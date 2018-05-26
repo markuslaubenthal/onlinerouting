c7f383b99254c44e76cbe7f0f6f30211025edc0a
Lennard Alms <mail@lennardalms.de>
2018-05-26 19:52:34 +0200

    createEdgesForCoan works now

--- tri.py
+++ tri.py
@@ -1,13 +1,14 @@
 from scipy.spatial import Delaunay
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines
 from matplotlib.widgets import Button
 from matplotlib.backend_bases import MouseEvent
 import math
 
 
 class Node:
-    def __init__(self, p, xp, x, y):
+    def __init__(self, p, xp, xpx, x, y):
         self.l = None
         self.r = None
         self.p = p
@@ -22,7 +23,7 @@ class Tree:
         self.vD2 = vD2
     def add(self, p, x, y):
         if(self.root == None):
-            self.root = Node(p, p, x, y)
+            self.root = Node(p, p, x, x, y)
         else:
             self._add(p, x, y, self.root)
 
@@ -34,7 +35,8 @@ class Tree:
         distD2_node = np.dot(np.array([node.x, node.y]), self.vD2) / np.dot(self.vD2, self.vD2)
         if distD2_new <= distD2_node:
             if node.l == None:
-                node.l = Node(p, p, x, y)
+                node.l = Node(p, p, x, x, y)
+                node.r = Node(node.p, node.p, node.x, node.x, node.y)
                 node.p = p
                 node.x = x
                 node.y = y
@@ -48,13 +50,13 @@ class Tree:
                     return (-1, 0, 0)
         else:
             if node.r == None:
-                node.r = Node(p, p, x, y)
+                node.r = Node(p, p, x, x, y)
+                node.l = Node(node.p, node.p, node.x, node.x, node.y)
                 return (p, x, y)
             else:
                 return self._add(p, x, y, node.r)
 
     def minAbove(self, p, x, y):
-        self.printTree()
         if(self.root == None):
             return 0
         else:
@@ -75,7 +77,7 @@ class Tree:
                 # _j = np.array([self._minAbove(p, x, y, node.l)])
                 # print _i
                 # print _j
-                return np.append(np.array([[node.xp, node.xpx]]), self._minAbove(p, x, y, node.l), axis = 0)
+                return np.append(np.array([[node.r.xp, node.r.xpx]]), self._minAbove(p, x, y, node.l), axis = 0)
             else:
                 return np.array([[node.xp, node.xpx]])
         else:
@@ -98,8 +100,8 @@ def createEdgesForCoan(P):
     #queryes T for r = minAbove(p) this returns the edge [p,r]
     #saves and returns edges
     #vD1 = np.array([2 * sqrt(3), -3])
-    vD1 = np.array([2 * math.sqrt(3), -3])
-    vD2 = np.array([2 * math.sqrt(3), 3])
+    vD1 = np.array([1, -math.sqrt(3)])
+    vD2 = np.array([1, math.sqrt(3)])
 
     #creating the viewing order
     unsortedList = []
@@ -108,15 +110,13 @@ def createEdgesForCoan(P):
         unsortedList.append([i, distD1])
     a = np.array(unsortedList)
     viewingOrder = a[a[:,1].argsort()]
-    viewingOrder = viewingOrder[:,0].astype(int)
-    print viewingOrder[::-1]
+    viewingOrder = viewingOrder[:,0].astype(int)[::-1]
     tree = Tree(vD2)
 
     for p in viewingOrder:
         tree.add(p, P[p][0], P[p][1])
         minAbove = tree.minAbove(p, P[p][0], P[p][1])
-        print "---"
-        print(minAbove, p)
+
 
 
 
@@ -155,7 +155,6 @@ class examplePlot(object):
         axes = plt.subplot(1, 1, 1)
         axes.set_xlim(0, 100)
         axes.set_ylim(0, 100)
-        #axes.grid(which="both")
         axes.plot(self._points[:,0], self._points[:,1], 'bo')
         self._axes = axes
         self._figure.canvas.mpl_connect('button_press_event', self._on_click)
@@ -175,6 +174,10 @@ class examplePlot(object):
     def _update_plot(self):
         self._trigangulation = Delaunay(self._points)
         tri = self._trigangulation
+        print tri.simplices
+        # for each line:
+        #line = lines.Line2D([30, 20], [20, 20])
+        #self._axes.add_line(line)
         pltpoints = np.copy(self._points)
         self._axes.cla()
         self._axes.set_ylim(0, 100)
